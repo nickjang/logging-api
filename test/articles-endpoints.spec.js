1a77707d684cef6894e44632ef6cfcb97b7bdb9c
nickjang <nickjang@alumni.upenn.edu>
2020-10-29 18:28:07 -0400

    Add POST, DELETE, routing, and xss sanitation

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,9 +1,9 @@
 const { expect } = require('chai');
 const knex = require('knex');
 const app = require('../src/app');
-const { makeArticlesArray } = require('./articles.fixtures');
+const { makeArticlesArray, makeMaliciousArticle } = require('./articles.fixtures');
 
-describe.only('Articles Endpoints', function () {
+describe('Articles Endpoints', function () {
   let db;
 
   before('make knex instance', () => {
@@ -44,6 +44,26 @@ describe.only('Articles Endpoints', function () {
           .expect(200, testArticles);
       });
     });
+
+    context('Given an XSS attack article', () => {
+      const { maliciousArticle, expectedArticle } = makeMaliciousArticle();
+
+      beforeEach('insert malicious article', () => {
+        return db
+          .into('blogful_articles')
+          .insert([maliciousArticle]);
+      });
+
+      it('removes XSS attack content', () => {
+        return supertest(app)
+          .get('/articles')
+          .expect(200)
+          .expect(res => {
+            expect(res.body[0].title).to.eql(expectedArticle.title);
+            expect(res.body[0].content).to.eql(expectedArticle.content);
+          });
+      });
+    });
   });
 
   describe('GET /articles/:article_id ', () => {
@@ -52,10 +72,10 @@ describe.only('Articles Endpoints', function () {
         const articleId = 123;
         return supertest(app)
           .get(`/articles/${articleId}`)
-          .expect(404, {error: {message: 'Article doesn\'t exist'}});
+          .expect(404, { error: { message: 'Article doesn\'t exist' } });
       });
     });
-    
+
     context('Given there are articles in the database', () => {
       const testArticles = makeArticlesArray();
 
@@ -71,6 +91,131 @@ describe.only('Articles Endpoints', function () {
         return supertest(app)
           .get(`/articles/${articleId}`)
           .expect(200, expectedArticle);
+      });
+    });
+
+    context('Given an XSS attack article', () => {
+      const { maliciousArticle, expectedArticle } = makeMaliciousArticle();
+
+      beforeEach('insert malicious article', () => {
+        return db
+          .into('blogful_articles')
+          .insert([maliciousArticle]);
+      });
+
+      it('removes XSS attack content', () => {
+        return supertest(app)
+          .get(`/articles/${maliciousArticle.id}`)
+          .expect(200)
+          .expect(res => {
+            expect(res.body.title).to.eql(expectedArticle.title);
+            expect(res.body.content).to.eql(expectedArticle.content);
+          });
+      });
+    });
+  });
+
+  describe('POST /articles', () => {
+    it('creates an article, responding with 201 and the new article', function () {
+      this.retries(3);
+      const newArticle = {
+        title: 'Test new article',
+        style: 'Listicle',
+        content: 'Test new article content...'
+      };
+
+      return supertest(app)
+        .post('/articles')
+        .send(newArticle)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.title).to.eql(newArticle.title);
+          expect(res.body.style).to.eql(newArticle.style);
+          expect(res.body.content).to.eql(newArticle.content);
+          expect(res.body).to.have.property('id');
+          expect(res.headers.location).to.eql(`/articles/${res.body.id}`);
+          const expected = new Date().toLocaleString();
+          const actual = new Date(res.body.date_published).toLocaleString();
+          expect(actual).to.eql(expected);
+        })
+        .then(postRes =>
+          supertest(app)
+            .get(`/articles/${postRes.body.id}`)
+            .expect(postRes.body)
+        );
+    });
+
+    const requiredFields = ['title', 'style', 'content'];
+
+    requiredFields.forEach(field => {
+      const newArticle = {
+        title: 'Test new article',
+        style: 'Listicle',
+        content: 'Test new article content...'
+      };
+
+      it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+        delete newArticle[field];
+
+        return supertest(app)
+          .post('/articles')
+          .send(newArticle)
+          .expect(400, {
+            error: { message: `Missing '${field}' in request body` }
+          });
+      });
+    });
+
+    context('Given an XSS attack article', () => {
+      const { maliciousArticle, expectedArticle } = makeMaliciousArticle();
+
+      beforeEach('insert malicious article', () => {
+        return db
+          .into('blogful_articles')
+          .insert([maliciousArticle]);
+      });
+
+      it('removes XSS attack content', () => {
+        return supertest(app)
+          .get(`/articles/${maliciousArticle.id}`)
+          .expect(200)
+          .expect(res => {
+            expect(res.body.title).to.eql(expectedArticle.title);
+            expect(res.body.content).to.eql(expectedArticle.content);
+          });
+      });
+    });
+  });
+
+  describe('DELETE /articles/:article_id', () => {
+    context('Given no articles', () => {
+      it('responds with 404', () => {
+        const articleId = 123;
+        return supertest(app)
+          .delete(`/articles/${articleId}`)
+          .expect(404, { error: { message: 'Article doesn\'t exist' } });
+      });
+    });
+    context('Given there are articles in the database', () => {
+      const testArticles = makeArticlesArray();
+
+      beforeEach('insert articles', () => {
+        return db
+          .into('blogful_articles')
+          .insert(testArticles);
+      });
+
+      it('responds with 204 and removes the article', () => {
+        const idToRemove = 2;
+        const expectedArticles = testArticles.filter(article => article.id !== idToRemove);
+        return supertest(app)
+          .delete(`/articles/${idToRemove}`)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get('/articles')
+              .expect(expectedArticles)
+          );
       });
     });
   });
